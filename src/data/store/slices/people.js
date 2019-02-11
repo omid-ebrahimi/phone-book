875df39c2724732701efa2096684a728e86d2167
Omid Ebrahimi <ebrahimi.omid69@gmail.com>
2019-02-11 15:44:57 +0330

    uuid used to make id for people

--- src/data/store/slices/people.js
+++ src/data/store/slices/people.js
@@ -1,9 +1,10 @@
 import {createSlice} from 'redux-starter-kit'
+import uuid from 'uuid/v4'
 
 // Reducers
 const addPerson = (state, action) => {
     const {name, defaultPhone} = action.payload;
-    state.push({name,  defaultPhone})
+    state.push({id: uuid(), name,  defaultPhone})
 };
 
 // Slice
