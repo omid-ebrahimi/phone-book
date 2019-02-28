5415470235c8d66731d454b2c1b32f3541a1f93f
Omid Ebrahimi <ebrahimi.omid69@gmail.com>
2019-02-28 18:45:38 +0330

    implements some parts of PeopleScreen and renders it in App component

---
+++ src/screens/PeopleScreen/component.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class PeopleScreen extends Component {
+    // For Test
+    componentDidMount() {
+        const personId =
+            this.props.addPerson('Omid Ebrahimi', '09104588100');
+        this.props.addPhone(personId, '09104588100', 'Mobile');
+    }
+
+    render() {
+        const {people} = this.props;
+        return (
+            <div>
+                {
+                    Object.values(people).map(
+                        person => (<div key={person.id}>{person.name}<br/></div>)
+                    )
+                }
+            </div>
+        );
+    }
+}
+
+PeopleScreen.propTypes = {
+    people: PropTypes.object.isRequired,
+    addPerson: PropTypes.func.isRequired,
+    addPhone: PropTypes.func.isRequired
+};
+
+export default PeopleScreen;
