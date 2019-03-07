8380bffb9f957aa84e98915163b455a8fc8da09e
Omid Ebrahimi <ebrahimi.omid69@gmail.com>
2019-03-07 16:26:27 +0330

    makes some refactorings

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import Provider from './data/store/provider';
 import PeopleScreen from './screens/PeopleScreen/container';
 import './App.css';
-import '@material/react-card/dist/card.css';
 
 class App extends Component {
     render() {
