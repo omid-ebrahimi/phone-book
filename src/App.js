131dcb539ba45b8752b1a4521cb0f4fa6134b635
Omid Ebrahimi <ebrahimi.omid69@gmail.com>
2019-03-07 16:37:08 +0330

    adds ReactRouter to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
 import Provider from './data/store/provider';
 import PeopleScreen from './screens/PeopleScreen/container';
 import './App.css';
@@ -7,7 +8,9 @@ class App extends Component {
     render() {
         return (
             <Provider>
-                <PeopleScreen/>
+                <BrowserRouter>
+                    <Route exact path="/" component={PeopleScreen} />
+                </BrowserRouter>
             </Provider>
         );
     }
