e08fb6abb80ac3fba1dbd20d682c8d8a49c27f39
Omid Ebrahimi <ebrahimi.omid69@gmail.com>
2019-04-11 15:48:58 +0430

    changes phoneTypes

--- src/data/types/phoneTypes.js
+++ src/data/types/phoneTypes.js
@@ -1,1 +1,10 @@
-export const phoneTypes = ['Mobile', 'Home', 'Work', 'Fax'];
+// @flow
+
+export const phoneTypes = {
+    Mobile: 'Mobile',
+    Home: 'Home',
+    Work: 'Work',
+    Fax: 'Fax'
+};
+
+export type PhoneType = $Keys<typeof phoneTypes>;
