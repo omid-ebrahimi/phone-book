9664c0386353f5eae0fe8c743b01acbcc867350b
Omid Ebrahimi <ebrahimi.omid69@gmail.com>
2019-05-03 12:22:57 +0430

    makes refactoring

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import '@material/react-card/dist/card.css';
-import '@material/react-fab/dist/fab.css';
-import '@material/react-material-icon/dist/material-icon.css';
-import '@material/react-text-field/dist/text-field.css';
 import App from './App';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
