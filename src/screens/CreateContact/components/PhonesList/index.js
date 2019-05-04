f2af54acde6017736f62d2100533999c726d1046
omid-ebrahimi <ebrahimi.omid69@gmail.com>
2019-05-04 17:35:45 +0430

    makes a style change in PhonesList screen

--- src/screens/CreateContact/components/PhonesList/index.js
+++ src/screens/CreateContact/components/PhonesList/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PhoneField from "./components/PhoneField";
 import MaterialIcon from "@material/react-material-icon";
-import {Headline4} from "@material/react-typography";
+import {Headline5} from "@material/react-typography";
 import IconButton from "@material/react-icon-button";
 
 export const PhonesList = ({phones, setPhoneNumber, setPhoneType, addPhone, removePhone}) => {
     return (
         <>
             <div className='flex justify-between items-center'>
-                <Headline4>Phones</Headline4>
+                <Headline5>Phones</Headline5>
                 <IconButton onClick={addPhone}>
                     <MaterialIcon icon="add"/>
                 </IconButton>
