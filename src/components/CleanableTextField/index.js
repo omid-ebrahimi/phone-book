7e451c532bc75a0d65ba087312c7b52a20b1b707
omid-ebrahimi <ebrahimi.omid69@gmail.com>
2019-05-11 16:24:38 +0430

    refactors CleanableTextField to focus after clean

--- src/components/CleanableTextField/index.js
+++ src/components/CleanableTextField/index.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import TextField from "@material/react-text-field";
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import TextField, {Input} from "@material/react-text-field";
 import MaterialIcon from "@material/react-material-icon";
 
-const CleanableTextField = ({clean, children, ...props}) => {
-    return (
-        <TextField {...props} onTrailingIconSelect={clean}
-                   trailingIcon={<MaterialIcon role="button" icon="delete"/>}>
-            {children}
-        </TextField>
-    );
+class CleanableTextField extends Component {
+    constructor(props) {
+        super(props);
+        this.textInput = React.createRef();
+    }
+
+    focusInput = () => {
+        if (!this.textInput) return;
+        const inputElement = this.textInput.inputElement;
+        if (inputElement) {
+            inputElement.focus();
+        }
+    };
+
+    cleanAndFocus = () => {
+        this.props.setValue('');
+        this.focusInput();
+    };
+
+    render() {
+        const {isValid, value, setValue, onBlur, ...props} = this.props;
+        return (
+            <TextField {...props} onTrailingIconSelect={this.cleanAndFocus}
+                       trailingIcon={<MaterialIcon role="button" icon="delete"/>}>
+                <Input value={value} onChange={(e) => setValue(e.currentTarget.value)}
+                       isValid={isValid} onBlur={onBlur} ref={input => this.textInput = input}/>
+            </TextField>
+        );
+    }
+}
+
+CleanableTextField.prototypes = {
+    setValue: PropTypes.func.isRequired,
+    value: PropTypes.string,
+    onBlur: PropTypes.func,
+    isValid: PropTypes.bool
 };
 
 export default CleanableTextField;
--- src/screens/CreateContact/components/ContactInfo/index.js
+++ src/screens/CreateContact/components/ContactInfo/index.js
@@ -1,42 +1,28 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import CleanableTextField from "../../../../components/CleanableTextField";
-import {HelperText, Input} from "@material/react-text-field";
+import {HelperText} from "@material/react-text-field";
 
 export class ContactInfo extends Component {
-    state = {
-        isValid: true
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            isValid: true
+        }
+    }
 
     validate(name) {
         this.props.nameSchema.isValid(name)
             .then(isValid => this.setState({isValid}))
     }
 
-    focusInput = () => {
-        if (!this.input) return;
-        const inputElement = this.input.inputElement;
-        if (inputElement) {
-            inputElement.focus();
-        }
-    };
-
-    cleanAndFocus() {
-        this.props.setContactName('');
-        this.focusInput();
-    }
-
     render() {
-        const {isValid} = this.state;
         const {name, setContactName} = this.props;
         return (
             <CleanableTextField className='col-12 mt1' outlined label='Full Name'
                                 helperText={<HelperText>Contact Full Name</HelperText>}
-                                clean={() => this.cleanAndFocus()}>
-                <Input value={name} onChange={(e) => setContactName(e.currentTarget.value)}
-                       isValid={isValid} onBlur={(e) => this.validate(e.currentTarget.value)}
-                       ref={input => this.input = input}/>
-            </CleanableTextField>
+                                value={name} setValue={(name) => setContactName(name)}
+                                isValid={this.state.isValid} onBlur={(e) => this.validate(e.currentTarget.value)}/>
         );
     }
 }
