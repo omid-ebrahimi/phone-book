65666a455d0e8ccf1d50f794a65d8660329b98f8
Omid Ebrahimi <ebrahimi.omid69@gmail.com>
2019-05-23 18:08:46 +0430

    replaces custom SelectMenu component with react-select

--- src/screens/CreateContact/components/PhonesList/components/PhoneField/index.js
+++ src/screens/CreateContact/components/PhonesList/components/PhoneField/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import TextField, {Input} from "@material/react-text-field";
 import {phoneTypes} from '../../../../../../data/types/phones';
-import SelectMenu from '../../../../../../components/SelectMenu';
 import MaterialIcon from "@material/react-material-icon";
 import IconButton from '@material/react-icon-button';
+import Select from '@material/react-select';
+
+const types = Object.values(phoneTypes);
 
 const PhoneField = ({phone: {number, type}, setPhoneType, removePhone, id, ...props}) => {
     return (
         <section className='flex justify-between items-center mb1'>
 
-            <TextField className='col-7'>
+            <TextField className='col-12'>
                 <Input {...props} value={number} id={`${id}.number`}/>
             </TextField>
 
-            <SelectMenu items={Object.values(phoneTypes)} selectedItem={type}
-                        onSelectItem={setPhoneType}/>
+            <Select
+                enhanced
+                className='ml1'
+                onEnhancedChange={(index) => setPhoneType(types[index])}
+                value={type}
+                options={types}
+            />
 
             <IconButton onClick={removePhone}>
                 <MaterialIcon icon="remove"/>
