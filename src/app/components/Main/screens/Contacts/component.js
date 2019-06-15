d06afc6925c5ae192b46e2431bfc425004f7a538
Omid Ebrahimi <ebrahimi.omid69@gmail.com>
2019-06-15 13:52:14 +0430

    refactors ContactsScreen styles

--- src/app/components/Main/screens/Contacts/component.js
+++ src/app/components/Main/screens/Contacts/component.js
@@ -1,22 +1,22 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import Card, {CardPrimaryContent} from '@material/react-card/dist/index';
-import {Fab} from '@material/react-fab/dist/index';
-import MaterialIcon from "@material/react-material-icon/dist/index";
-import './component.css';
+import Card, {CardPrimaryContent} from '@material/react-card';
+import {Fab} from '@material/react-fab';
+import MaterialIcon from "@material/react-material-icon";
 import {Link} from "react-router-dom";
+import styles from './index.module.css';
 
 class ContactsScreen extends Component {
     render() {
         const {contacts} = this.props;
         return (
             <>
-                <section className='cards-container'>
+                <section className='flex flex-wrap justify-center'>
                     {
                         Object.values(contacts).map(
                             contact => (
-                                <Card key={contact.id} className='card'>
-                                    <CardPrimaryContent className='card-primary-content'>
+                                <Card key={contact.id} className='m1'>
+                                    <CardPrimaryContent className={styles.cardPrimaryContent}>
                                         <h1>{contact.name}</h1>
                                         <p>Phone: {contact.defaultPhone}</p>
                                     </CardPrimaryContent>
@@ -24,7 +24,7 @@ class ContactsScreen extends Component {
                         )
                     }
                 </section>
-                <div className='fab-container'>
+                <div className={`col-12 flex justify-center fab-container ${styles.fabContainer}`}>
                     <Link to='/contacts/create'><Fab textLabel='Add Contact' icon={<MaterialIcon icon="add"/>}/></Link>
                 </div>
             </>
